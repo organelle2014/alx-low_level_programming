62783345043cf7fe852af7b5983ae352f106e28f
organelle2014 <richiejune@yahoo.com>
2023-02-16 08:00:34 -0800

    6-size.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main method starts here
  *
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
         return (0);
 }
