221117a3e1be2cf77c0be44504f43907b49f758e
organelle2014 <richiejune@yahoo.com>
2023-02-16 18:10:35 +0300

    second trial

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include<stdio.h>
+/*the header is above
+ * the main functions is below
+ * goal - to display a message
+ */
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
