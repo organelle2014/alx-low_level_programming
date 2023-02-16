7f5aec6c658b1ad89fba70f8c35f8bcb48a06a15
organelle2014 <richiejune@yahoo.com>
2023-02-16 08:11:25 -0800

    this one is tough

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * using printf in themain method
- * 0 if success on the return
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
 int main(void)
 {
