043a96ddf3b7d901ba30621a221b356b50856e59
organelle2014 <richiejune@yahoo.com>
2023-02-16 17:53:12 +0300

    This is my first file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,9 @@
-#!/bin/bash
+#include<sdio.h>
+/**
+ * main method starts here
+ * the aim is to get an output
+ */
+int main(){
+	printf("Programming is like building a multilingual puzzle");
+	return 0;
+}
