c4e94c1ffb1073b5b334eb7d1137d2263b967c54
organelle2014 <richiejune@yahoo.com>
2023-02-16 08:03:54 -0800

    Really betty??

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/* *
- * to output using printf
- * has a return method
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
