ff890f9452a4b6d4c3c4a914bbadad0e02f19aa8
organelle2014 <richiejune@yahoo.com>
2023-02-17 10:10:59 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		putchar(x);
+		x++;
+	}
+	putchar ('\n');
+	return (0);
+
+}
